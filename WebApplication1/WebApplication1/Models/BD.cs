6774ae7ab0f5b656907a03fbf3285e02058b6a25
LUCAS FACUNDO CONFORTI <44759529@comunidadort.edu.ar>
2019-10-17 11:52:34 -0300

    Cree el backoffice, bd y clase skateparks

---
+++ WebApplication1/WebApplication1/Models/BD.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+namespace WebApplication1.Models
+{
+    public class BD
+    {
+        private static SqlConnection Conectar()
+        {
+            string strConn = "Server=.;Database =TP PAGINA;Trusted_Connection=True;";
+            SqlConnection a = new SqlConnection(strConn);
+            a.Open();
+            return a;
+        }
+        private static void Desconectar(SqlConnection conn)
+        {
+            conn.Close();
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/SkateParks.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class SkateParks
+    {
+        private int idSkatePark;
+        private string Nombre;
+        private string Imagen;
+        private string Desc;
+        private bool Destacado;
+
+
+
+        public HttpPostedFileBase Foto { get; set; }
+        public int _IdSkatePark { get => idSkatePark; set => idSkatePark = value; }
+        public string _Nombre { get => Nombre; set => Nombre = value; }
+        public string _Imagen { get => Imagen; set => Imagen = value; }
+        public string _Desc { get => Desc; set => _Desc = value; }
+        public bool _Destacado { get => Destacado; set => Destacado = value; }
+
+        public SkateParks()
+        {
+            idSkatePark = 0;
+            Nombre = "";
+            Imagen = "";
+            Desc = "";
+            Destacado = false;
+        }
+
+        public SkateParks(int a, string b, string c, string d, bool e)
+        {
+            idSkatePark = a;
+            Nombre = b;
+            Imagen = c;
+            Desc = d;
+            Destacado = e;
+        }
+    }
+}
